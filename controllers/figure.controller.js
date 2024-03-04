63e34a769285c0ccc9f300a0441e322e58a468ea
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-03-04 15:38:20 +0100

    Ajouter le middleware Multer et mettre à jour les dépendances

--- controllers/figure.controller.js
+++ controllers/figure.controller.js
@@ -1,10 +1,11 @@
 const db = require("../models");
-const { Blob } = require("@vercel/blob");
+const { put } = require("@vercel/blob");
 const Figure = db.figure;
 const Op = db.Sequelize.Op;
 
 // Création et enregistrement d'une nouvelle figure
 exports.create = async (req, res) => {
+    console.log(req.body);
     // Validation de la requête
     if (!req.body.title) {
         res.status(400).json({ message: "Le contenu ne peut pas être vide !" });
@@ -12,14 +13,17 @@ exports.create = async (req, res) => {
     }
 
     // Supposons que req.file contient les données de l'image
-const blob = Blob.from(req.file.buffer);
-const id = await blob.write();
-console.log(`Blob stored with id: ${id}`);
+    if (!req.file) {
+        res.status(400).json({ message: "L'image est requise !" });
+        return;
+    }
+    const blob = await put(req.file.originalname, req.file.buffer, { access: 'public' });
+    console.log(`Blob stored with id: ${blob.id}`);
     // Création d'une figure
     const figure = {
         title: req.body.title,
         description: req.body.description,
-        image: id, // stockez l'ID du blob ici
+        image: blob.id, // stockez l'ID du blob ici
         video: req.body.video
     };
 
@@ -29,22 +33,5 @@ console.log(`Blob stored with id: ${id}`);
         res.send(data);
     } catch (err) {
         res.status(500).json({ message: err.message || "Une erreur s'est produite lors de la création de la figure." });
-    }
-};
-
-// Récupération de toutes les figures de la base de données
-exports.findAll = async (req, res) => {
-    try {
-        const figures = await Figure.findAll();
-        for (let figure of figures) {
-            if (figure.image) {
-                const blob = await Blob.read(figure.image);
-                const buffer = await blob.arrayBuffer();
-                figure.image = `data:image/jpeg;base64,${Buffer.from(buffer).toString('base64')}`;
-            }
-        }
-        res.send(figures);
-    } catch (err) {
-        res.status(500).json({ message: err.message || "Une erreur s'est produite lors de la récupération des figures." });
     }
 };
--- routes/figure.routes.js
+++ routes/figure.routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const figures = require("../controllers/figure.controller.js");
+const multer = require('../middlewares/multer.config.js');
 
-router.post("/", figures.create);
+router.post("/",multer, figures.create);
 router.get("/", figures.findAll);
 
 module.exports = router;
