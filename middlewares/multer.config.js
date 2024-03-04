f87bbb5b9ce021b119e6b8aa4d856888df3324cf
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-03-04 18:14:41 +0100

    Mettre à jour la configuration de multer

--- middlewares/multer.config.js
+++ middlewares/multer.config.js
@@ -1,9 +1,13 @@
 const multer = require('multer');
 
-const storage = multer.memoryStorage();
-const upload = multer({ storage }).fields([
-  { name: 'image', maxCount: 1 },
-  { name: 'video', maxCount: 1 }
+const upload = multer({
+    storage: multer.memoryStorage(),
+    limits: {
+        fileSize: 15 * 1024 * 1024, // Limite à 15MB
+    }
+}).fields([
+    { name: 'image', maxCount: 1 },
+    { name: 'video', maxCount: 1 }
 ]);
 
 module.exports = upload;
