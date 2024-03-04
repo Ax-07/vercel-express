0f5552b336db827ac4537499d6d83fd2b26b7df1
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-03-04 12:05:41 +0100

    Ajouter des options SSL pour la connexion à la base de données PostgreSQL

--- models/index.js
+++ models/index.js
@@ -3,7 +3,13 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.POSTGRES_URL, {
     dialect: 'postgres',
-    dialectModule: require('pg')
+    dialectModule: require('pg'),
+    dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      }
 });
 const db = {}; // Création d'un objet vide pour stocker les modèles de la base de données
 
